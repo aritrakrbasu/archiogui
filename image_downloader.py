11849d2c000fc3d5e4d2a0ff5fcb95764f47f25f
AnanyaDasgupta <ananyadasgupta280@gmail.com>
2021-11-20 14:05:43 +0530

    update

--- image_downloader.py
+++ image_downloader.py
@@ -1,6 +1,6 @@
 from bing_image_downloader import downloader
 import pandas as pd
-sheet_name = 'aritra'
+sheet_name = 'Ananya'
 file_name = 'records.xlsx'
 df = pd.read_excel(file_name, sheet_name=sheet_name)
 place_names = df.name_en.tolist()
