3929855b581299521341a551b9992aa13e4065a1
Aritra Basu <aritrabasu71@gmail.com>
2021-11-20 13:51:25 +0530

    login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Col, Form, Row, } from 'react-bootstrap'
+import Signup from './routes/Signup/Signup';
 function App() {
   return (
-    <Row>
-      <Col xl={1} className="bg-danger">a</Col>
-      <Col xl={7} className="bg-info">b</Col>
-    </Row>
+    <Signup />
   );
 }
 
---
+++ frontend/src/routes/Signup/Signup.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import './Signup.css'
+import {Col, Form, Row, } from 'react-bootstrap'
+import user from './user.png'
+import logoLarge from './logoLarge.png'
+import logoSmall from './logoSmall.png'
+import Button from '@restart/ui/esm/Button'
+function Signup() {
+    return (
+       <div className="signupContainer">
+           <div className="signupContainerOverlay">
+           <div className="logoContainer">
+               <img src={logoSmall} className="logoSmall" />
+           </div>
+               <Row>
+                   <Col lg={6} className="leftContainer">
+                       <div className="leftContainerInner">
+
+                        <h1 className="textheader">
+                                Create an account <span></span>
+                        </h1>
+                        <img src={user} className="userIcon"/>
+                            <Form>
+                                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                                        <Form.Label className="styledLabel">Name</Form.Label>
+                                        <Form.Control type="text" placeholder="Name" className="styledInput" />
+                                    </Form.Group>
+                                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                                        <Form.Label className="styledLabel">Email address</Form.Label>
+                                        <Form.Control type="email" placeholder="Enter email" className="styledInput" />
+                                    </Form.Group>
+
+                                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                                        <Form.Label className="styledLabel">Password</Form.Label>
+                                        <Form.Control type="password" placeholder="Password" className="styledInput"/>
+                                    </Form.Group>
+                                    <Button variant="primary" className="smallbtn" type="submit">
+                                        Login
+                                    </Button>
+                                    <Button variant="primary" className="largebtn" type="submit">
+                                        Signup Now
+                                    </Button>
+                            </Form>
+                            </div>
+                   </Col>
+                   <Col lg={6} className="rightContainer">
+                       <img src={logoLarge}  className="bigLogo"/>
+                   </Col>
+               </Row>
+           </div>
+       </div>
+    )
+}
+
+export default Signup
