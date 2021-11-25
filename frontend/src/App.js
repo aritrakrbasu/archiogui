dbc34d77a264086f5e8953414f8072fd8ed3f563
Anushree Pal <anushreepal291101@gmail.com>
2021-11-26 03:09:55 +0530

    LOGIN AND FORGET PWD UI ADDED

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Signup from './routes/Signup/Signup';
+import Login from './routes/Login/Login';
+import Forgetpwd from './routes/Forgetpwd/Forgetpwd';
 function App() {
   return (
-    <Signup />
+    
+    <Forgetpwd />
   );
 }
 
