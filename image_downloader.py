18e30f7b0d705b081f967ce24face1fd06c5fd4e
Soham Sarkar <sohamsarkarv1.0@gmail.com>
2021-11-27 14:54:08 +0530

    Image Downloader Script Update

--- image_downloader.py
+++ image_downloader.py
@@ -19,26 +19,37 @@ def renameImages(place):
         os.rename(src, dst)
 
 
-sheet_name = '' #Update sheet name
+sheet_name = 'SoH' #Update sheet name
 file_name = 'records.xlsx'
 df = pd.read_excel(file_name, sheet_name=sheet_name)
 place_names = df.name_en.tolist()
 
 for place in place_names:
-    current_dir = ['Train', 'Test', 'Validation']
+    current_dir = [{
+        "name": 'Train',
+        "lim": 200
+    }, {
+        "name": "Validation",
+        "lim": 100
+    }]
     try:
         os.mkdir("Dataset/Download/" + place)
     except:
         print("Already exists")
     for dir in current_dir:
-        os.mkdir("Dataset/" + dir + "/" + place)
+        os.mkdir("Dataset/" + dir['name'] + "/" + place)
     index = 0
-    lim = 6 # Should be divisible by 3
+    lim = 300
     arguments = {"keywords": place, "limit": lim, "print_urls": True, "format": "jpg"}
     absolute_image_paths = response.download(arguments)
     renameImages(place)
     # downloader.download(place, limit=lim,  output_dir='Dataset/Download', adult_filter_off=True, force_replace=False, timeout=60)
-    for i in range(0, lim, lim // 3):
-        moveImages(i, i + lim//3, current_dir[index], place)
-        index += 1
+    # for i in range(0, lim, lim // 3):
+    #     moveImages(i, i + lim//3, current_dir[index], place)
+    #     index += 1
+    start = 0
+    for index in range(0, 2):
+        moveImages(start, start + current_dir[index]['lim'], current_dir[index]['name'], place)
+        start += current_dir[index]['lim']
+
     os.rmdir('Dataset/Download/' + place)
