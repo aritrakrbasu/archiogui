d2e281d8fd94cb2a75f97c350a4fd5734661a81e
Aritra Basu <aritrabasu71@gmail.com>
2021-12-04 13:13:48 +0530

    data added to firebase

--- frontend/src/routes/Dashboard/Dashboard.js
+++ frontend/src/routes/Dashboard/Dashboard.js
@@ -1,72 +1,77 @@
-import React, {useEffect, useState} from 'react'
-import { Col, Row } from 'react-bootstrap'
-import Header from '../../Components/Header'
-import Navbar from '../../Components/Navbar'
-import PageHeader from '../../Components/PageHeader'
-import PlaceItem from '../../Components/PlaceItem'
-import PopularPlacesItem from '../../Components/PopularPlacesItem'
-import ProfileShort from '../../Components/ProfileShort'
-import './Dashboard.css'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import { Col, Row } from "react-bootstrap";
+import Header from "../../Components/Header";
+import Navbar from "../../Components/Navbar";
+import PageHeader from "../../Components/PageHeader";
+import PlaceItem from "../../Components/PlaceItem";
+import PopularPlacesItem from "../../Components/PopularPlacesItem";
+import ProfileShort from "../../Components/ProfileShort";
+import "./Dashboard.css";
+import axios from "axios";
 function Dashboard() {
-    const [placeDetails, setplaceDetails] = useState([])
-    const [popularPlaces, setpopularPlaces] = useState([]) 
+	const [placeDetails, setplaceDetails] = useState([]);
+	const [popularPlaces, setpopularPlaces] = useState([]);
 
-    useEffect(() => {
-        axios.get("https://archiogui.herokuapp.com/getallplaces").then(data=>{
-           if(data.status===200) {
-                var places = data.data.places
-                setplaceDetails(places)
-           }
-       })
-    }, [])
+	useEffect(() => {
+		axios.get("https://archiogui.herokuapp.com/getallplaces").then((data) => {
+			if (data.status === 200) {
+				var places = data.data.places;
+				setplaceDetails(places);
+			}
+		});
+	}, []);
 
-    useEffect(() => {
-            var dupPlaceDetails = [...placeDetails]
-            var randomPlaces = []
-            var count = placeDetails.length > 8 ?8 : placeDetails.length
-            for(let i=0;i<count;i++) {
-                console.log(dupPlaceDetails.length)
-                var randomNumber = Math.floor(Math.random()*dupPlaceDetails.length)
-                randomPlaces.push(dupPlaceDetails[randomNumber])
-                dupPlaceDetails.splice(randomNumber,1)
-            }
-            
-            setpopularPlaces(randomPlaces)
-    }, [placeDetails])
-    
-    return (
-        <>
-        <Row className="pageWrap">
-                <Col>
-                    <Header/>
-                </Col>
-                <Col>
-                    <Navbar />
-                </Col>
+	useEffect(() => {
+		var dupPlaceDetails = [...placeDetails];
+		var randomPlaces = [];
+		var count = placeDetails.length > 8 ? 8 : placeDetails.length;
+		for (let i = 0; i < count; i++) {
+			var randomNumber = Math.floor(Math.random() * dupPlaceDetails.length);
+			randomPlaces.push(dupPlaceDetails[randomNumber]);
+			dupPlaceDetails.splice(randomNumber, 1);
+		}
 
-            <ProfileShort/>
-            <PageHeader text={"Popular Places"} />
-           { popularPlaces && popularPlaces.length > 0 && popularPlaces.map((place)=>{
-               return(<PopularPlacesItem image={place?.image} place_name ={place?.place_name}/>)
-           })
-               
-           }
-           
-            <PageHeader text={"Other Places"}/>
-            {
-                placeDetails && placeDetails.length > 0 && placeDetails.map((place)=>{
-                    return(<PlaceItem image={place.image} place_name={place.place_name} location ={place.state} /> )
-                })
-            }
-            {
-                placeDetails && placeDetails.length > 0 && placeDetails.map((place)=>{
-                    return(<PlaceItem image={place.image} place_name={place.place_name} location ={place.state} /> )
-                })
-            }
-        </Row>
-        </>
-    )
+		setpopularPlaces(randomPlaces);
+	}, [placeDetails]);
+
+	return (
+		<>
+			<Row className='pageWrap'>
+				<Col>
+					<Header />
+				</Col>
+				<Col>
+					<Navbar />
+				</Col>
+
+				<ProfileShort />
+				<PageHeader text={"Popular Places"} />
+				{popularPlaces &&
+					popularPlaces.length > 0 &&
+					popularPlaces.map((place) => {
+						return (
+							<PopularPlacesItem
+								image={place?.image}
+								place_name={place?.place_name}
+							/>
+						);
+					})}
+
+				<PageHeader text={"Other Places"} />
+				{placeDetails &&
+					placeDetails.length > 0 &&
+					placeDetails.map((place) => {
+						return (
+							<PlaceItem
+								image={place.image}
+								place_name={place.place_name}
+								location={place.state}
+							/>
+						);
+					})}
+			</Row>
+		</>
+	);
 }
 
-export default Dashboard
+export default Dashboard;
